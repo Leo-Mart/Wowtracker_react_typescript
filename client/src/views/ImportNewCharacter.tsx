a4625b3cfff44b5d75271d010269ba26fdb910a3
Martin Leo <martin.christoffer.leo@gmail.com>
2025-09-28 16:41:05 +0200

    some work on the import page

--- client/src/views/ImportNewCharacter.tsx
+++ client/src/views/ImportNewCharacter.tsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
+
 const ImportNewCharacter = () => {
+  const [characterName, setCharacterName] = useState("");
+  const [characterRealm, setCharacterRealm] = useState("");
+  const [characterRegion, setCharacterRegion] = useState("eu");
+
+  const handleImportClick = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = {
+      name: characterName,
+      realm: characterRealm,
+      region: characterRegion,
+    };
+
+    const response = await fetch("/api/characters/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+    const result = await response.json();
+
+    if (response.status == 201) {
+      //TODO: add some form of toast or similar to give feedback. Also clear the form or navigate to a new page, maybe to the newly added character?
+      console.log(result);
+    } else {
+      // do something
+    }
+  };
   return (
     <>
       <div className="bg-gray-900 dark:bg-gray-900 grid grid-cols-3 gap-2">
         <h2 className="text-2xl font-bold text-gray-900 md:text-3xl dark:text-white col-start-2">
           Import new character
         </h2>
-        <form className="mt-8 col-start-2">
+        <form className="mt-8 col-start-2" onSubmit={handleImportClick}>
           {/* TODO: add region selector*/}
           <label
-            htmlFor="name"
+            htmlFor="character-name"
             className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-emerald-600 focus-within:ring-1 focus-within:ring-emerald-600 dark:border-gray-700 dark:bg-gray-800"
           >
             <span className="text-xs font-medium text-gray-700 dark:text-gray-200">
@@ -16,14 +45,15 @@ const ImportNewCharacter = () => {
             </span>
             <input
               type="text"
-              name="name"
+              name="character-name"
               placeholder="enter character name"
-              id="char-name"
+              value={characterName}
+              onChange={(e) => setCharacterName(e.target.value)}
               className="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm dark:text-white"
             />
           </label>
           <label
-            htmlFor="realm"
+            htmlFor="character-realm"
             className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-emerald-600 focus-within:ring-1 focus-within:ring-emerald-600 dark:border-gray-700 dark:bg-gray-800 mt-4"
           >
             <span className="text-xs font-medium text-gray-700 dark:text-gray-200">
@@ -31,9 +61,10 @@ const ImportNewCharacter = () => {
             </span>
             <input
               type="text"
-              name="realm"
+              name="character-realm"
               placeholder="enter character realm"
-              id="char-realm"
+              value={characterRealm}
+              onChange={(e) => setCharacterRealm(e.target.value)}
               className="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm dark:text-white"
             />
           </label>
@@ -45,7 +76,6 @@ const ImportNewCharacter = () => {
           </button>
         </form>
       </div>
-      <div className="bg-gray-900 flex justify-center " id="characters"></div>
     </>
   );
 };
